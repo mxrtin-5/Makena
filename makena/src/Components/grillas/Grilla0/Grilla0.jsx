4909132d8f1778b57fbe1a9afc5294ec84711c43
mxrtin-5 <martineluney023@gmail.com>
2023-09-07 16:45:36 -0300

    Grillas

--- makena/src/Components/grillas/Grilla0/Grilla0.jsx
+++ makena/src/Components/grillas/Grilla0/Grilla0.jsx
@@ -8,6 +8,14 @@ const Grilla0 = ({ phoneImg }) => {
 
     const [imgData, setImgData] = useState("");
 
+    const [escala, setEscala] = useState(1)
+
+    const [translateX, setTranslateX] = useState(0)
+
+    const [translateY, setTranslateY] = useState(0)
+
+    const [zIndex, setZIndex] = useState(true)
+
     console.log(imgData);
 
     const [isPopupOpen, setPopupOpen] = useState(false)
@@ -48,18 +56,36 @@ const Grilla0 = ({ phoneImg }) => {
     return (
         <>
             <div className={styles.marco}>
-                <img className={styles.marcoImg} src={phoneImg} alt="" />
-                <img className={isPopupOpen ? styles.imagen : styles.imagenConBorde} src={imgData && imgData.url} />
-
-
-
+                <img style={{
+                    zIndex: zIndex ? 1000 : 1
+                }} className={styles.marcoImg} src={phoneImg} alt="" />
+                <img style={{
+                   transform: `scale(${escala}) translate(${translateX}px, ${translateY}px)`
+                }} className={isPopupOpen ? styles.imagen : styles.imagenConBorde} src={imgData && imgData.url} />
             </div>
 
             <UploadWidget getImageData={setImgData} />
 
-            
-            <button className={styles.button2} onClick={TogglePopup}>Editar posiciones</button>
+            <div className={styles.containerEditar}>
+                <div className={styles.container}>
+                    <button className={styles.button} onClick={() => setEscala(escala + 0.1)}>Zoom +</button>
+                    <button className={styles.button} onClick={() => setEscala(escala - 0.1)}>Zoom -</button>
+                </div>
 
+                <div className={styles.container}>
+                    <button className={styles.button} onClick={() => setTranslateX(translateX + 3)}>{"<="}</button>
+                    <button className={styles.button} onClick={() => setTranslateX(translateX - 3)}>{"=>"}</button>
+                </div>
+
+                <div className={styles.container}>
+                    <button className={styles.button} onClick={() => setTranslateY(translateY + 3)}>{"Arriba"}</button>
+                    <button className={styles.button} onClick={() => setTranslateY(translateY - 3)}>{"Abajo"}</button>
+                </div>
+
+            </div>
+
+
+            <button className={styles.button2} onClick={() => setZIndex(!zIndex)}>Ocultar</button>
         </>
     );
 }
