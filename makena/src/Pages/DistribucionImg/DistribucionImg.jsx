4e107e1b72ed8fd168f9708c5f9f69d771a137cb
mxrtin-5 <martineluney023@gmail.com>
2023-11-09 13:52:40 -0300

    Ajustes distribucion

--- makena/src/Pages/DistribucionImg/DistribucionImg.jsx
+++ makena/src/Pages/DistribucionImg/DistribucionImg.jsx
@@ -4,7 +4,7 @@ import grilla1 from "/grilla1.png"
 import grilla2 from "/grilla2.png"
 import grilla3 from "/grilla3.png"
 import grilla4 from "/grilla4.png"
-import grilla5 from "/grilla5.png"
+import styles from './Distribucion.module.css'
 
 
 const DistribucionImg = () => {
@@ -14,7 +14,7 @@ const DistribucionImg = () => {
     const modelo = location.search.split("=")[2]
 
     return (
-        <section>
+        <section className={styles.sectionContainer}>
             <Link to={`/crear-funda/seleccion/:${modelo}/${id}?tipoGrilla=${0}`}>
                 <img src={Grilla0} alt="grillaImage" />
             </Link>
@@ -29,9 +29,6 @@ const DistribucionImg = () => {
             </Link>
             <Link to={`/crear-funda/seleccion/:${modelo}/${id}?tipoGrilla=${4}`}>
                 <img src={grilla4} alt="grillaImage" />
-            </Link>
-            <Link to={`/crear-funda/seleccion/:${modelo}/${id}?tipoGrilla=${5}`}>
-                <img src={grilla5} alt="" />
             </Link>
         </section>
     );
