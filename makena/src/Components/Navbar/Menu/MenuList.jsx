8db5cde9bd327fc28ba87d078b62f94fb2d02684
mxrtin-5 <martineluney023@gmail.com>
2023-11-29 17:13:02 -0300

    Ajustes responsive

--- makena/src/Components/Navbar/Menu/MenuList.jsx
+++ makena/src/Components/Navbar/Menu/MenuList.jsx
@@ -25,7 +25,7 @@ const Menulist = ({ cerrar }) => {
 
             <nav ref={navRef} onClick={handleCloseMenu} className='menulist'>
                 <Link onClick={showNavbar} className="menulist__link active" to="/">Inicio</Link>
-                <Link onClick={showNavbar} className="menulist__link" to="/crear-funda/seleccion">Crear Funda</Link>
+                <Link onClick={showNavbar} className="menulist__link" to="/crear-funda/seleccion">Crea tu funda</Link>
                 <Link onClick={showNavbar} className="menulist__link" to="/contacto">Contacto</Link>
 
                 <button className="nav-btn nav-close-btn" onClick={handleCloseMenu}>
