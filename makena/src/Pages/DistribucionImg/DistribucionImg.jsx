09a8693ae981f746918d91856793036eefec3c52
mxrtin-5 <martineluney023@gmail.com>
2023-11-29 17:12:30 -0300

    Cambio imagenes select grillas

--- makena/src/Pages/DistribucionImg/DistribucionImg.jsx
+++ makena/src/Pages/DistribucionImg/DistribucionImg.jsx
@@ -1,9 +1,9 @@
 import { Link, useLocation } from "react-router-dom";
-import Grilla0 from '/Grilla0.png'
-import grilla1 from "/grilla1.png"
-import grilla2 from "/grilla2.png"
-import grilla3 from "/grilla3.png"
-import grilla4 from "/grilla4.png"
+import Grilla0 from '/Grilla1.png'
+import grilla1 from "/Grilla2.png"
+import grilla2 from "/Grilla3.png"
+import grilla3 from "/Grilla4.png"
+import grilla4 from "/Grilla5.png"
 import styles from './Distribucion.module.css'
 
 
@@ -15,21 +15,24 @@ const DistribucionImg = () => {
 
     return (
         <section className={styles.sectionContainer}>
-            <Link to={`/crear-funda/seleccion/:${modelo}/${id}?tipoGrilla=${0}`}>
-                <img src={Grilla0} alt="grillaImage" />
-            </Link>
-            <Link to={`/crear-funda/seleccion/:${modelo}/${id}?tipoGrilla=${1}`}>
-                <img src={grilla1} alt="grillaImage" />
-            </Link>
-            <Link to={`/crear-funda/seleccion/:${modelo}/${id}?tipoGrilla=${2}`}>
-                <img src={grilla2} alt="grillaImage" />
-            </Link>
-            <Link to={`/crear-funda/seleccion/:${modelo}/${id}?tipoGrilla=${3}`}>
-                <img src={grilla3} alt="grillaImage" />
-            </Link>
-            <Link to={`/crear-funda/seleccion/:${modelo}/${id}?tipoGrilla=${4}`}>
-                <img src={grilla4} alt="grillaImage" />
-            </Link>
+            <div className={styles.containerGrid}>
+                <Link className={styles.link} to={`/crear-funda/seleccion/:${modelo}/${id}?tipoGrilla=${0}`}>
+                    <img src={Grilla0} alt="grillaImage" />
+                </Link>
+                <Link className={styles.link} to={`/crear-funda/seleccion/:${modelo}/${id}?tipoGrilla=${1}`}>
+                    <img src={grilla1} alt="grillaImage" />
+                </Link>
+                <Link className={styles.link} to={`/crear-funda/seleccion/:${modelo}/${id}?tipoGrilla=${2}`}>
+                    <img src={grilla2} alt="grillaImage" />
+                </Link>
+                <Link className={styles.link} to={`/crear-funda/seleccion/:${modelo}/${id}?tipoGrilla=${3}`}>
+                    <img src={grilla3} alt="grillaImage" />
+                </Link>
+                <Link className={styles.link} to={`/crear-funda/seleccion/:${modelo}/${id}?tipoGrilla=${4}`}>
+                    <img src={grilla4} alt="grillaImage" />
+                </Link>
+            </div>
+
         </section>
     );
 }
