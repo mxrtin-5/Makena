45697b6ac67f4ccff527025f181ca88b37bf082f
Alfonzo Hernandez <111934617+kattae23@users.noreply.github.com>
2023-12-06 23:10:03 -0400

    si

--- makena/src/App.jsx
+++ makena/src/App.jsx
@@ -44,6 +44,7 @@ function App() {
                   <Navbar />
 
                   <Routes>
+                    <Route path='home' element={<Home />} />
                     <Route path="*" element={<Navigate to="/home" />} />
                     <Route path="/" element={<Navigate to="/home" />} />
                     <Route path='/crear-funda/seleccion' element={<CrearFunda />} />
